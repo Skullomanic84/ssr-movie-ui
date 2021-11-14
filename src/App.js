7ce9a49659078e5f0983e52539affe0deb33f65d
Skullomanic84 <jhshiloh@gmail.com>
2021-11-14 17:12:55 +0200

    Added GlobalStyle.js
    files created GlobalStyle.js
    Added the .env file with the movie API Key connected.
    GlobalStyle.js imported to App.js file.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
 
+//global styles
+import { GlobalStyle } from './GlobalStyle'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      start 🙉 
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/GlobalStyle.js
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export const GlobalStyle = createGlobalStyle `
+
+:root {
+    --maxWidth: 1440px;
+    --white: #fff;
+    --lightGrey: #eee;
+    --medGrey: #353535;
+    --darkGrey: #1c1c1c;
+    --fontSuperBig: 2.5rem;
+    --fontBig: 1.5rem;
+    --fontMed: 1.2rem;
+    --fontSmall: 1rem;
+}
+
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+
+body {
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+  font-family: "Work Sans", sans-serif;
+  font-weight: 400;
+  font-size: 0.8125rem;
+
+    h1 {
+        font-size: 2rem;
+        font-weight: 600;
+        color: var(--white);
+        }
+
+    h3 {
+        font-size: 1.1.rem;
+        font-weight: 600;
+        }
+
+    p {
+        font-size: 1rem;
+        color: var(--white);
+        }
+
+    img,
+    picture {
+    max-width: 100%;
+    display: block;
+        }
+
+    input,
+    button,
+    textarea,
+    select {
+    font: inherit;
+        }
+
+}
+
+`
