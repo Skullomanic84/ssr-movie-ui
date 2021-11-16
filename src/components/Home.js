192cf6955ec98d983d421c61956fb1bcb3fcbeed
Skullomanic84 <jhshiloh@gmail.com>
2021-11-16 11:34:17 +0200

    Added HeroImage\index.js
    Created HeroImage folder
    Added index.js
    Added HeroImage.styles.js and updated the Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 
 //config
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL }from '../config'
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL }from '../config';
 
 //components
-
+import HeroImage from './HeroImage';
 
 //hook
-import { useHomeFetch } from '../hooks/useHomeFetch'
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 //Image
-import NoImage from '../images/no_image.jpg'
+import NoImage from '../images/no_image.jpg';
 
 function Home() {
 
@@ -20,10 +20,16 @@ function Home() {
     console.log(state);
 
     return (
-        <div>
-            Home page
-        </div>
-    )
-}
+        <>
+            {state.results[0] ? (
+                <HeroImage
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+                />
+            ) : null}
+        </>
+    );
+};
 
 export default Home
