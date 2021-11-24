2b6da5697a8a7f30066f37235ec01ecf3a687c97
Skullomanic84 <jhshiloh@gmail.com>
2021-11-24 15:38:11 +0200

    updated Movie.js
    Created custom hook useMovieFetch.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+//components
+import Grid from './Grid';
+import Spinner from './Spinner';
+
+//hooks
+import { useMovieFetch } from '../hooks/useMovieFetch';
+
+//image
+import NoImage from '../images/no_image.jpg';
 
 function Movie() {
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log(movie);
     return (
-        <div>
-            
-        </div>
+        <>
+            <div>Movie</div>
+        </>
     )
 }
 
